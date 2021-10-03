c2dbf3d2ea218c92207e62e52d93c9898a69f0e2
Clara <zixuany@andrew.cmu.edu>
2021-10-03 17:43:33 -0400

    enable popping up obstacles

---
+++ Crucible/Assets/Minigames/PuzzleRun/Scripts/FWW.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FWW : Obstacle
+{
+
+    void Start()
+    {
+        startPosition = transform.position;
+        heights[0] = 0;
+        heights[1] = 1;
+        heights[2] = 1;
+    }
+
+    void Update()
+    {
+        if (isOnScreen) {
+            transform.position -= speed * Time.deltaTime;
+        }
+        else {
+            transform.position = startPosition;
+        }
+    }
+
+}
